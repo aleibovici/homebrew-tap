a05f1d41a54262abeea9248839d5c2a508434304
aleibovici <ableibovici@gmail.com>
2026-09-17 19:23:56 +1200

    grammarfix 1.1

--- Casks/grammarfix.rb
+++ Casks/grammarfix.rb
@@ -1,6 +1,6 @@
 cask "grammarfix" do
-  version "1.0"
-  sha256 "a25b08573d109a3de796a38fbffb8a02bee5cf5a3b0370998028086b3238c0ff"
+  version "1.1"
+  sha256 "a9fa3383c646b8e058ed4266d0733598f697293dea94ba1ec7f288a72fe0935c"
 
   url "https://github.com/aleibovici/grammarfix/releases/download/v#{version}/GrammarFix-#{version}.zip"
   name "GrammarFix"
